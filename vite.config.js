8fa50cbcca6a12336b4fbfc7b2055ba074205485
Kevin Joseph <kj.joseph@gmail.com>
2024-12-12 13:55:29 -0800

    Ignore module errors on build

--- vite.config.js
+++ vite.config.js
@@ -4,23 +4,33 @@ import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    watch: {
-        usePolling: true
-    }
-  },
-  resolve: {
-    alias: {
-        "@": path.resolve(__dirname, "./src"),
-    }
-  },
-  css: {
+	plugins: [react()],
+	server: {
+		watch: {
+				usePolling: true
+		},
+	},
+	resolve: {
+		alias: {
+				"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	css: {
 	preprocessorOptions: {
 		scss: {
 			api: "modern-compiler",
 			silenceDeprecations: ["legacy-js-api", "mixed-decls", "color-functions"],
-		}
+		},
 	}
-  }
+	},
+	build: {
+	rollupOptions: {
+		onwarn(warning, warn) {
+			if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
+				return;
+			}
+			warn(warning);
+		},
+	},
+	},
 });
