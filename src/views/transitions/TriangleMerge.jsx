03fac5f268dc895883fa60a38cc13313e285d780
Kevin Joseph <kj.joseph@gmail.com>
2024-12-15 15:04:54 -0800

    Fix image locations

--- src/views/transitions/TriangleMerge.jsx
+++ src/views/transitions/TriangleMerge.jsx
@@ -15,33 +15,21 @@ const TriangleMerge = (props) => {
 			</div>
 			<div className="trianglesRight">
 			</div>
-			<div className="logoLeft">
-				<div className="logo">
-					<img src={`/images/logos/${props.transition.logo}`}></img>
-				</div>
-			</div>
-			<div className="logoRight">
-				<div className="logo">
-					<img src={`/images/logos/${props.transition.logo}`}></img>
-				</div>
-			</div>
-
-
-
-
-{/* 			<div className="bg">
-				{props.transition.logo && props.transition.logo !== null && props.transition.logo !== "" ? (
-					<div className="logo">
-						<img src={`/images/logos/${props.transition.logo}`}></img>
+			{props.transition.logo ?
+				<>
+					<div className="logoLeft">
+						<div className="logo">
+							<img src={`/images/logos/${props.transition.logo}`}></img>
+						</div>
 					</div>
-				): null}
-			</div>
-			<div className="stripe">
-				{props.transition.text ? (
-					<div className="text">{props.transition.text}</div>
-				) : null}
-			</div>
- */}
+					<div className="logoRight">
+						<div className="logo">
+							<img src={`/images/logos/${props.transition.logo}`}></img>
+						</div>
+					</div>
+				</>
+			: null}
+
 		</>
 	)
 
