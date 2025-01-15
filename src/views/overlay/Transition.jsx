bd68a29ad2fd26ed0ced84df7f15ca39ef30f639
Kevin Joseph <kj.joseph@gmail.com>
2025-01-14 17:31:12 -0800

    Missed a file when modifying transition timing

--- src/views/overlay/Transition.jsx
+++ src/views/overlay/Transition.jsx
@@ -6,7 +6,12 @@ import TriangleMerge from "@/views/transitions/TriangleMerge";
 const Transition = (props) => {
 
 	return (
-		<div id="Transition" className={`${props.transition.name} ${props.transition.delay ? "delay" : ""} ${props.transition.show ? "show" : ""} ${props.transition.team !== null ? `team${props.transition.team}` : ""}`}>
+		<div id="Transition"
+			className={`${props.transition.name} ${props.transition.show ? "show" : ""} ${props.transition.team !== null ? `team${props.transition.team}` : ""}`}
+			style={{
+				"--transitionDelay": `${props.transition.delay}s`,
+			}}
+		>
 
 			{props.transition.name === "stripeWipe" ?
 
